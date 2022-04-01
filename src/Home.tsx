efd584471abb6d55f3e07fcb9f8b672e1f4781ec
Iamad <solmapsol@gmail.com>
2022-04-01 22:01:41 +0200

    Fe

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import confetti from "canvas-confetti";
 import * as anchor from "@project-serum/anchor";
